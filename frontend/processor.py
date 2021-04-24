d3268a7042b1d24c31b9955ff2ceaa7be8607c68
Ragify <73241561+Ragify@users.noreply.github.com>
2021-04-25 00:16:02 +0530

    added post feature

--- frontend/processor.py
+++ frontend/processor.py
@@ -2,18 +2,15 @@ import json
 import os
 
 def converter():
-    cmd = '''curl.exe -X POST -F images=@./frontend/static/dog.png "http://localhost:5000/detections"'''
+    cmd = '''curl.exe -X POST -F images=@./frontend/static/image.png "http://localhost:5000/detections"'''
     stream = os.popen(cmd)
     try:
         data = json.loads(stream.read())
-        print(type(data))
-        # data = json.loads(stream.read())
     except:
         return []
-    print(data,'hola')
     l=[]
     detections=data['response'][0]
     for i in detections['detections']:
-        if i['confidence'] > 90:
+        if i['confidence']:
             l.append(i['class'])
     return list(set(l))
